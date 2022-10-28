83e5b957ac04e66cd0fbb03d0d81311a50c279d5
AlicjaBobko <alicja_bobko@o2.pl>
2022-10-28 11:21:54 +0200

    style: about component

---
+++ components/About.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { motion } from "framer-motion";
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen  flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        src="https://i.ibb.co/zPKVkjz/Screenshot-3.png"
+        className="-mb-20 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+      />
+      <div className="space-y-10">
+        <h4>some bcg</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          accusamus fugiat repellat dicta consectetur, temporibus quidem modi
+          expedita nisi laboriosam libero. Necessitatibus, ducimus voluptas
+          veritatis sunt soluta ex nostrum itaque.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Head from "next/head";
+import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-proximity overflow-scroll z-0 scroll-smooth">
       <Head>
         <title>Aliz's stuff</title>
         <meta name="description" content="Portfolio" />
@@ -13,16 +14,18 @@ const Home: NextPage = () => {
           name="keyword"
           content="portfolio, javascript, typescript, nextjs, tailwind, developer"
         />
-
         <link rel="icon" href="/rainbow.ico" />
       </Head>
 
       <Header />
-      {/* Hero */}
-      <section id="hero" className="snap-center">
+
+      <section id="hero" className="snap-start ">
         <Hero />
       </section>
-      {/* About */}
+
+      <section id="about" className="snap-center  ">
+        <About />
+      </section>
       {/* Experience */}
       {/* Skills */}
       {/* Projects */}
